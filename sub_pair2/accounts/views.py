847d270d4bea6417d7d00d98a18e0cd8c87e0f2a
hjkim1350 <hjkim1350@daum.net>
2022-10-21 11:24:21 +0900

    HJ1 SIGNUP 초기 설정 완료

--- sub_pair2/accounts/views.py
+++ sub_pair2/accounts/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from django.shortcuts import render, redirect
+
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+
 
 # Create your views here.
 def index(request):
     return render(request, 'accounts/index.html')
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
