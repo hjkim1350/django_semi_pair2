329197550b9aeba75536a5fe9d66817595734d4e
devhum7p <devhum7p@gmail.com>
2022-10-21 13:35:57 +0900

    articles/update

--- sub_pair2/articles/views.py
+++ sub_pair2/articles/views.py
@@ -39,3 +39,22 @@ def detail(request, pk):
     }
 
     return render(request, 'articles/detail.html', context)
+
+
+def update(request, pk):
+
+    article = Article.objects.get(pk=pk)
+
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
+       
+    context = {
+        'article_form' : article_form,
+    }
+
+    return render(request, 'articles/update.html', context)
