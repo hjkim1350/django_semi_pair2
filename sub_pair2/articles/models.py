3c36d4ce37d3e0c25191fe17a552805cb8630f97
devhum7p <devhum7p@gmail.com>
2022-10-21 14:08:25 +0900

    articles/models 변경

--- sub_pair2/articles/models.py
+++ sub_pair2/articles/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from config import settings
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=20)
-    contents = models.TextField()
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=50)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 
