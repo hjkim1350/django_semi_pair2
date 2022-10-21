8cdf53dc01dafecc5d3e7d11219320bf0e784647
hjkim1350 <hjkim1350@daum.net>
2022-10-21 10:42:45 +0900

    gitignore 추가 및 README 위치 수정

--- sub_pair2/accounts/views.py
+++ sub_pair2/accounts/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, 'accounts/index.html')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
