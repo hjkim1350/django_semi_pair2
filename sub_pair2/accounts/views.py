8eb4974fcf91452450df42ee25aa58234647c408
hjkim1350 <hjkim1350@daum.net>
2022-10-21 14:13:05 +0900

    HJ3-accounts 페이지 구현 완료 / 회원가입, 로그인, 로그아웃, 회원탈퇴, 회원정보 보기, 비밀번호 변경

--- sub_pair2/accounts/views.py
+++ sub_pair2/accounts/views.py
@@ -1,8 +1,9 @@
-from contextlib import redirect_stderr
 from django.shortcuts import render, redirect
-
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model, update_session_auth_hash
 
 
 # Create your views here.
@@ -25,4 +26,62 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'accounts:index')
+    else:
+        form = AuthenticationForm()
+        
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:index')
+
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/update.html', context)
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/change_password.html', context)
+
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('accounts:index')
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/detail.html', context)
